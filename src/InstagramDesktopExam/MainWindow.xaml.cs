d6b1b9fe3cce10131f30044ab4f4d28b9b26db84
azizsayfullayev <azizsayfullayev1@gmail.com>
2022-10-05 11:27:19 +0500

    Added wpf

---
+++ src/InstagramDesktopExam/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿
+using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Controls;
+
+using System.Windows.Input;
+
+
+namespace InstagramDesktopExam
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textSearch_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtSearch.Focus();
+        }
+
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtSearch.Text) && txtSearch.Text.Length > 0)
+            {
+                textSearch.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textSearch.Visibility = Visibility.Visible;
+            }
+                
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Image files|*.bmp;*.jpg;*.png";
+            openDialog.FilterIndex = 1;
+
+            if (openDialog.ShowDialog() == true)
+            {
+                
+            }
+
+        }
+
+        public void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void message(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("No massages yet");
+        }
+    }
+}
